d640326bf0284b5dc790f7ba52021ffd1bdd032e
NIZAR RASYIID <nizarrasyiid1221@gmail.com>
2023-07-13 20:08:40 +0700

    baru

--- resources/views/admin/Input/suplierStore.blade.php
+++ resources/views/admin/Input/suplierStore.blade.php
@@ -1,30 +1,35 @@
 @extends('admin.admin')
 @section('admin')
-    <div class="page-content">
-
-        <form action="{{ url('/admin-table/storeSuplier') }}" method="POST" class="d-flex flex-column p-5 mt-5">
+    <div class="page-content pt-5">
+      <div class="card mt-5">
+        <div class="card-body">
+          <h5 class="card-title">Input Suplier/Vendor</h5>
+          <form action="{{ url('/admin-table/storeSuplier') }}" method="POST" class="d-flex flex-column px-5 ">
             @csrf
             <div class="mb-3">
                 <div class="form-group">
                   <div class="input-group">
                     <span class="input-group-text">Nama</span>
-                    <input type="text" class="form-control" id="nama_suplier" name="nama_suplier" required>
+                    <input type="text" class="form-control border border-secondary" id="nama_suplier" name="nama_suplier" required>
                   </div>
                 </div>
                 <div class="form-group mt-3">
                     <div class="input-group">
                       <span class="input-group-text">Nomor Telepon</span>
-                    <input type="number" class="form-control" id="nomor_telepon" name="nomor_telepon" required>
+                    <input type="number" class="form-control border border-secondary" id="nomor_telepon" name="nomor_telepon" required>
                     </div>
                 </div>
                 <div class="form-group mt-3">
                   <div class="input-group">
                     <span class="input-group-text">Alamat</span>
-                    <input type="text" class="form-control" id="alamat_suplier" name="alamat_suplier" required>
+                    <input type="text" class="form-control border border-secondary" id="alamat_suplier" name="alamat_suplier" required>
                   </div>
                 </div>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
+        </div>
+      </div>
+
     </div>
 @endsection
